16c2f68c6b25437d78aa77f01cf345372033bec8
gDoe24 <gregkeel11@gmail.com>
2023-10-22 18:41:29 -0400

    refactor: logging

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -95,12 +95,12 @@ def index():
         vote1 = r.get(button1).decode('utf-8')
         # tracer object to trace cat vote
         with tracer.span(name='catVote') as span:
-            span.add_annotation('Vote for Cat')
+            logger.info("Cats Vote")
 
         vote2 = r.get(button2).decode('utf-8')
         # tracer object to trace dog vote
         with tracer.span(name='dogVote') as span:
-            span.add_annotation('Vote for Dog')
+            logger.info("Dogs Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -116,7 +116,6 @@ def index():
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # logger object to log dog vote
             logger.info("Cats Vote Total", extra=properties)
-
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
